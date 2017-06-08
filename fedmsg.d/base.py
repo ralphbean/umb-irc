4e8edb92bb888357c45906a5fd9876ab0959cf1d
Ralph Bean <rbean@redhat.com>
2017-06-07 21:09:44 -0400

    Some configuration.

---
+++ fedmsg.d/base.py
@@ -0,0 +1,13 @@
+import os
+
+config = dict(
+    environment="dev",
+    zmq_enabled=False,
+    stomp_uri='messaging-devops-broker01.dev1.ext.devlab.redhat.com:61612,messaging-devops-broker02.dev1.ext.devlab.redhat.com:61612',
+    stomp_heartbeat=1000,
+    stomp_user=os.environ['UMB_USERNAME'],
+    stomp_pass=os.environ['UMB_PASSWORD'],
+    stomp_ssl_crt='/opt/app-root/umb-dev.crt',
+    stomp_ssl_key='/opt/app-root/umb-dev.key',
+    stomp_queue='/queue/Consumer.rbean.VirtualTopic.eng.>',
+)
