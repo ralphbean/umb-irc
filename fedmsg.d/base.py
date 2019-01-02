43b9b19f6b0f866189b51eaead5d30309986a05c
Ralph Bean <rbean@redhat.com>
2019-01-02 15:27:53 -0500

    Pesky dot delimited key names.

--- fedmsg.d/base.py
+++ fedmsg.d/base.py
@@ -8,7 +8,6 @@ config = dict(
     environment="dev",
     zmq_enabled=False,
     # 'messaging-devops-broker01.dev1.ext.devlab.redhat.com:61612,messaging-devops-broker02.dev1.ext.devlab.redhat.com:61612',
-    blocking_mode=blocking_mode,
     stomp_heartbeat=heartbeat,
     stomp_uri=os.environ['UMB_URI'],
     stomp_user=os.environ['UMB_USERNAME'],
@@ -17,3 +16,5 @@ config = dict(
     stomp_ssl_key='/secrets/key',
     stomp_queue='/queue/Consumer.client-umb-irc.umb-irc.VirtualTopic.eng.>',
 )
+
+config['moksha.blocking_mode'] = blocking_mode
